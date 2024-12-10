cbe05ad2c13b6ef14b5cf664eeb889c3a8712aff
root <root@Chacha.>
2024-12-10 17:14:23 +0100

    Modified the code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *left_node = binary_tree_node(NULL, value);
+	binary_tree_t *left_node;
 
 	if (parent == NULL)
 		return (NULL);
+
+	left_node = binary_tree_node(NULL, value);
 
 	if (left_node == NULL)
 		return (NULL);
